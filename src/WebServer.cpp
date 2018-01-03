4362f087c7449d6d6b2a04cb098ff5bf31737315
Alexander Schulz <alex@nope.bz>
2018-01-03 18:53:55 +0100

    Implement the index page, add a flashbag

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -9,31 +9,98 @@ WebServer::WebServer(int port) {
 }
 
 WebServer::~WebServer(void) {
-  for (size_t i; i < this->__endpoints_count; i++) free(this->__endpoints[i]);
+  for (size_t i = 0; i < this->__endpoints_count; i++) free(this->__endpoints[i]);
   free(this->__endpoints);
 }
 
 void WebServer::__index_fn(void) {
-  // TODO: implement dat shit yo!
+  String content = this->getFlashbag();
+  content.concat(this->__index_content_prefix);
+
+  content.concat("<p class='endpoints'>");
+  for (size_t i = 0; i < this->__endpoints_count; i++) {
+    content.concat("<a href='");
+    content.concat(this->__endpoints[i]->path);
+    content.concat("'><code>");
+    content.concat(this->__endpoints[i]->path);
+    content.concat("</code></a> - ");
+    content.concat(this->__endpoints[i]->description);
+    content.concat("<br />");
+  }
+  content.concat("</p>");
+
+  this->send(200, "text/html", content.c_str());
 }
 
-void WebServer::setIndexPath(String path) {
-  if (this->__index_path == path) {
-    this->on(this->__index_path.c_str(), HTTP_ANY, NULL);
+void WebServer::setIndexPath(const char *path) {
+  if (strcmp(this->__index_path, path) == 0) {
+    this->on(this->__index_path, HTTP_ANY, NULL);
     this->__index_path = path;
-    this->on(this->__index_path.c_str(), HTTP_ANY, [=](void){ this->__index_fn(); });
+    this->on(this->__index_path, HTTP_ANY, [=](void){ this->__index_fn(); });
   }
 }
 
-String WebServer::getIndexPath(void) {
+const char* WebServer::getIndexPath(void) {
   return this->__index_path;
 }
 
-void WebServer::addEndpoint(String path, String description, THandlerFunction fn) {
-  this->addEndpoint(path, description, HTTP_ANY, fn);
+void WebServer::setIndexContentPrefix(const char *prefix) {
+  this->__index_content_prefix = String(prefix);
 }
 
-void WebServer::addEndpoint(String path, String description, HTTPMethod method, THandlerFunction fn) {
+String& WebServer::getIndexContentPrefix(void) {
+  return this->__index_content_prefix;
+}
+
+void WebServer::addFlash(const char *message) {
+  this->addFlash(NULL, message);
+}
+
+void WebServer::addFlash(const char *level, const char *message) {
+  if (message &&  message[0] != '\0') {
+    if (level && level[0] != '\0') {
+      this->__flashbag.concat("<li class='flash flash-");
+      this->__flashbag.concat(level);
+      this->__flashbag.concat("'>");
+    } else {
+      this->__flashbag.concat("<li class='flash'>");
+    }
+    this->__flashbag.concat(message);
+    this->__flashbag.concat("</li>");
+  }
+}
+
+String WebServer::getFlashbag() {
+  if (this->__flashbag.length() == 0) return "";
+
+  String flashbag =
+    "<style>"
+      "ul.flashbag {"
+        "padding: 0;"
+        "list-style: none;"
+      "}"
+      "ul.flashbag li {"
+        "padding: 10px;"
+        "background: #efeeee;"
+        "border: 1px solid white;"
+      "}"
+    "</style>"
+    "<ul class='flashbag'>";
+
+  flashbag.concat(this->__flashbag);
+  flashbag.concat("</li>");
+
+  this->__flashbag = "";
+
+  return flashbag;
+}
+
+int WebServer::addEndpoint(const char *path, const char *description, THandlerFunction fn) {
+  return this->addEndpoint(path, description, HTTP_ANY, fn);
+}
+
+int WebServer::addEndpoint(const char *path, const char *description, HTTPMethod method, THandlerFunction fn) {
+  if (path == NULL || path[0] == '\0' || path[0] != '/') return 1;
 
   struct webserver_endpoint* endpoint = (webserver_endpoint*) malloc(sizeof(webserver_endpoint));
   endpoint->path = path;
@@ -45,5 +112,24 @@ void WebServer::addEndpoint(String path, String description, HTTPMethod method, 
   this->__endpoints = (webserver_endpoint**) realloc(this->__endpoints, this->__endpoints_count * sizeof(struct webserver_endpoint*));
   this->__endpoints[this->__endpoints_count - 1] = endpoint;
 
-  this->on(path.c_str(), method, fn);
+  this->on(path, method, fn);
+
+  return 0;
+}
+
+void WebServer::send(int code, const char *content_type, const String &content) {
+  boolean send_flashbag = (strcmp(content_type, "text/html") == 0 && (code % 400) < 100);
+
+  this->send(code, content_type, content, send_flashbag);
+}
+
+void WebServer::send(int code, const char *content_type, const String &content, boolean send_flashbag) {
+  if (send_flashbag) {
+    String content_with_flashbag = this->getFlashbag();
+    content_with_flashbag.concat(content);
+
+    ESP8266WebServer::send(code, content_type, content_with_flashbag);
+  } else {
+    ESP8266WebServer::send(code, content_type, content);
+  }
 }
--- src/WebServer.h
+++ src/WebServer.h
@@ -6,8 +6,8 @@ class WebServer;
 #include <ESP8266WebServer.h>
 
 typedef struct webserver_endpoint {
-  String path;
-  String description;
+  const char *path;
+  const char *description;
   HTTPMethod method;
   ESP8266WebServer::THandlerFunction callback;
 } webserver_endpoint;
@@ -16,7 +16,9 @@ class WebServer : public ESP8266WebServer {
 private:
   struct webserver_endpoint** __endpoints;
   size_t __endpoints_count = 0;
-  String __index_path;
+  const char *__index_path;
+  String __index_content_prefix;
+  String __flashbag;
 
   void __index_fn(void);
 
@@ -24,10 +26,17 @@ public:
   WebServer(IPAddress addr, int port = 80);
   WebServer(int port = 80);
   ~WebServer(void);
-  void setIndexPath(String path);
-  String getIndexPath(void);
-  void addEndpoint(String path, String description, THandlerFunction fn);
-  void addEndpoint(String path, String description, HTTPMethod method, THandlerFunction fn);
+  void setIndexPath(const char *path);
+  const char* getIndexPath(void);
+  void setIndexContentPrefix(const char *prefix);
+  String& getIndexContentPrefix(void);
+  void addFlash(const char *message);
+  void addFlash(const char *level, const char *message);
+  String getFlashbag(void);
+  int addEndpoint(const char *path, const char *description, THandlerFunction fn);
+  int addEndpoint(const char *path, const char *description, HTTPMethod method, THandlerFunction fn);
+  void send(int code, const char *content_type, const String &content);
+  void send(int code, const char *content_type, const String &content, boolean send_flashbag);
 };
 
 #endif // __LORA_ESPER_WEBSERVER_H__
